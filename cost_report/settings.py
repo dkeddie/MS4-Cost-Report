756f797b1fae776771b6f2e1f7800983e8f219e3
dkeddie <mail@davidkeddie.com>
2021-04-06 19:02:55 +0100

    Pre-deploy to Heroku

--- cost_report/settings.py
+++ cost_report/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ms4-cost-report.herokuapp.com',
+    'localhost',
+    'http://127.0.0.1:8000/'
+]
 
 
 # Application definition
