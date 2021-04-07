ab96a6079c714d810db8f170c67cce35e5bb97ec
dkeddie <mail@davidkeddie.com>
2021-04-07 07:30:36 +0100

    Fix: attempt to sort staticfiles

--- cost_report/settings.py
+++ cost_report/settings.py
@@ -36,7 +36,7 @@ DEBUG = "DEVELOPMENT" in os.environ
 ALLOWED_HOSTS = [
     'ms4-cost-report.herokuapp.com',
     'localhost',
-    'http://127.0.0.1:8000/'
+    '127.0.0.1',
 ]
 
 
@@ -185,7 +185,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
