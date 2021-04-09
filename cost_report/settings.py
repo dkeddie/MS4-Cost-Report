6770495cb053e060800e511fdc9d6143186edd95
dkeddie <mail@davidkeddie.com>
2021-04-09 09:07:27 +0100

    Fix: Review email settings to attempt fix

--- cost_report/settings.py
+++ cost_report/settings.py
@@ -221,13 +221,13 @@ STRIPE_PLAN_YEARLY_ID = os.getenv('STRIPE_PLAN_YEARLY_ID')
 
 # Email Settings
 if 'DEVELOPMENT' in os.environ:
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = 'dkeddie.appdev@gmail.com'
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_PORT = 587
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
