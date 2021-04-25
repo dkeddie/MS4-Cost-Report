658100209c8305e363215b4d8d7a4754ec8c5ef6
dkeddie <mail@davidkeddie.com>
2021-04-25 20:35:07 +0100

    Update Heroku

--- cost_report/settings.py
+++ cost_report/settings.py
@@ -152,7 +152,6 @@ else:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
