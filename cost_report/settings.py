0fdebeefdebc513cf63c21a006f97604454501dc
dkeddie <mail@davidkeddie.com>
2021-04-26 08:09:26 +0100

    Fix: settings.py for email

--- cost_report/settings.py
+++ cost_report/settings.py
@@ -241,6 +241,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     EMAIL_PORT = 587
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
